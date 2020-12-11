f3910bbf622c3364e65908ff4cd2d027b446f822
Jordi-Guardia <jordiguardiav@gmail.com>
2020-12-11 17:33:04 +0100

    android basics network curs 4 carpeta 1 video 11 primera part feta, el codi ja funciona amb la integració de word.java i wordAdapter.java

--- app/src/main/java/com/example/android/quakereport/MainActivity.java
+++ app/src/main/java/com/example/android/quakereport/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.android.quakereport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,22 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("San Francisco", "Feb 2 2016");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        ArrayList<Word> earthquakes = new ArrayList<>();
+        earthquakes.add(new Word("7,2","San Francisco", "Feb 2, 2016"));
+        earthquakes.add(new Word("6,1","London", "july 20, 2015"));
+        earthquakes.add(new Word("3,9","Tokyo", "Nov 10, 2014"));
+        earthquakes.add(new Word("5,4","Mexico city", "May 3, 2014"));
+        earthquakes.add(new Word("2,8","Moscow", "Jan 31, 2013"));
+        earthquakes.add(new Word("4,9","Rio de Janeiro", "Aug 19, 2012"));
+        earthquakes.add(new Word("1,6","Paris", "Oct 30, 2011"));
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
         // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
-
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+          //      this, android.R.layout.list_items, earthquakes);
+        WordAdapter adapter = new WordAdapter(this, earthquakes);
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
---
+++ app/src/main/java/com/example/android/quakereport/Word.java
@@ -0,0 +1,41 @@
+package com.example.android.quakereport;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+    private final String mvalorQuake;
+    private final String mcity;
+    private final String mdateQuake;
+
+    /**
+     * @param valorQuake Valor màxim del seisme
+     * @param city Ciutat del seisme
+     * @param dateQuake Data del seisme
+     */
+    public Word(String valorQuake, String city, String dateQuake) {
+        mvalorQuake = valorQuake;
+        mcity = city;
+        mdateQuake = dateQuake;
+    }
+
+    /**
+     * @return És un string
+     */
+    public String getvalorQuake() {
+        return mvalorQuake;
+    }
+
+    /**
+     * @return És un string
+     */
+    public String getcity() {
+        return mcity;
+    }
+
+    /**
+     * @return És un String
+     */
+    public String getdateQuake() { return mdateQuake; }
+}
